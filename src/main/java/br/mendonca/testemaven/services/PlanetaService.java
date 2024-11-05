2b08259783daf341e593551ea1cfae15a627f7f0
Arthur <arthur.sampaio@upe.br>
2024-11-05 15:47:59 -0300

    feat: criando serviço dos planetas

--- src/main/java/br/mendonca/testemaven/services/PlanetaService.java
+++ src/main/java/br/mendonca/testemaven/services/PlanetaService.java
@@ -1,4 +1,36 @@
 package br.mendonca.testemaven.services;
 
+import br.mendonca.testemaven.dao.PlanetaDAO;
+import br.mendonca.testemaven.model.entities.Planeta;
+import br.mendonca.testemaven.services.dto.PlanetaDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanetaService {
+
+    public void adicionar(String nome, int densidade, boolean possuiAgua) throws ClassNotFoundException, SQLException {
+        PlanetaDAO dao = new PlanetaDAO();
+
+        Planeta planeta = new Planeta();
+        planeta.setNome(nome);
+        planeta.setDensidade(densidade);
+        planeta.setPossuiAgua(possuiAgua);
+
+        dao.adicionar(planeta);
+    }
+
+    public List<PlanetaDTO> listAllPlanetas() throws ClassNotFoundException, SQLException {
+        ArrayList<PlanetaDTO> resp = new ArrayList<PlanetaDTO>();
+
+        PlanetaDAO dao = new PlanetaDAO();
+        List<Planeta> lista = dao.listAllPlanetas();
+
+        for (Planeta planeta : lista) {
+            resp.add(PlanetaDTO.planetaMapper(planeta));
+        }
+
+        return resp;
+    }
 }
