d1388393c3b2a5dd6eebb4428e9a36b9900304b2
Arthur <arthur.sampaio@upe.br>
2024-11-05 15:45:34 -0300

    feat: adicionando entidade dos planetas

--- src/main/java/br/mendonca/testemaven/model/entities/Planeta.java
+++ src/main/java/br/mendonca/testemaven/model/entities/Planeta.java
@@ -1,4 +1,42 @@
 package br.mendonca.testemaven.model.entities;
 
+import java.util.UUID;
+
 public class Planeta {
+    private UUID uuid;
+    private String nome;
+    private int densidade;
+    private boolean possuiAgua;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDensidade() {
+        return densidade;
+    }
+
+    public void setDensidade(int densidade) {
+        this.densidade = densidade;
+    }
+
+    public boolean isPossuiAgua() {
+        return possuiAgua;
+    }
+
+    public void setPossuiAgua(boolean possuiAgua) {
+        this.possuiAgua = possuiAgua;
+    }
 }
