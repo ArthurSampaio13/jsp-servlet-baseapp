c8e06438dc24041037e4100234ea700de3e511b5
PierreOF <marcos.pierre@upe.br>
2024-11-19 15:11:56 -0300

    feat: userDTO com id

--- src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
+++ src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
@@ -2,10 +2,13 @@ package br.mendonca.testemaven.services.dto;
 
 import br.mendonca.testemaven.model.entities.User;
 
+import java.util.UUID;
+
 public class UserDTO {
 	
 	private String name;
 	private String email;
+	private String userId;
 	
 	public String getName() {
 		return name;
@@ -19,12 +22,20 @@ public class UserDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
 	public static UserDTO userMapper(User user) {
 		UserDTO dto = new UserDTO();
 		dto.setName(user.getName());
 		dto.setEmail(user.getEmail());
+		dto.setUserId(user.getUuid());
 		
 		return dto;
 	}
