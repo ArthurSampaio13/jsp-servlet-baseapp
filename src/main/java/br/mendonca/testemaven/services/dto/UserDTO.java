1b4b507f19520bd49f08e597cc4f979a2cd4fe40
Arthur <arthur.sampaio@upe.br>
2024-11-19 15:49:38 -0300

    feat: adicionando idade e status ao DTO usuario

--- src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
+++ src/main/java/br/mendonca/testemaven/services/dto/UserDTO.java
@@ -6,6 +6,8 @@ public class UserDTO {
 	
 	private String name;
 	private String email;
+	private int idade;
+	private boolean status;
 	
 	public String getName() {
 		return name;
@@ -19,12 +21,29 @@ public class UserDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
 	public static UserDTO userMapper(User user) {
 		UserDTO dto = new UserDTO();
 		dto.setName(user.getName());
 		dto.setEmail(user.getEmail());
+		dto.setIdade(user.getIdade());
+		dto.setStatus(user.isStatus());
 		
 		return dto;
 	}
